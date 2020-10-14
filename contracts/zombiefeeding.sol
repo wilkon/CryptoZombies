e0ef70ca663775572263394a42db420fb2551921
wilkon <tb.wilkon@gmail.com>
2020-10-14 01:00:25 -0400

    ZombieFeeding => feedandmultiply

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -3,5 +3,11 @@ pragma solidity >= 0.5.0 < 0.6.0;
 import "./zombiefactory.sol';
 
 contract ZombieFeeding is ZombieFactory{
-    
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+    }
 }
