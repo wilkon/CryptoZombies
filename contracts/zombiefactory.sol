89e8981b92f66425a893ee2a92052b50d95dfb45
wilkon <tb.wilkon@gmail.com>
2020-10-16 19:04:06 -0400

    setKittyContract => onlyOwner

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,6 +1,8 @@
 pragma solidity >= 0.5.0 < 0.6.0;
 
-contract ZombieFactory {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ZombieFactory is Ownable{
     event NewZombie(uint zombieId, string name, uint dna);
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
