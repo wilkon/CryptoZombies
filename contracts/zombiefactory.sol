50440d6c576fbe56480ada29ace99e0fe044ef33
wilkon <tb.wilkon@gmail.com>
2020-10-16 19:43:09 -0400

    ZombieFactory => time/level added to Zombie

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -6,10 +6,13 @@ contract ZombieFactory is Ownable{
     event NewZombie(uint zombieId, string name, uint dna);
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
     
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombies[] public zombies;
@@ -18,7 +21,7 @@ contract ZombieFactory is Ownable{
     mapping(address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
