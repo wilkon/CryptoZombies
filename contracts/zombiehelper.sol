bc3bbc51045d3838b91b0197cdfd46d4cbacbb01
wilkon <tb.wilkon@gmail.com>
2020-10-22 01:32:35 -0400

    zombiehelper => change name/dna

---
+++ contracts/zombiehelper.sol
@@ -0,0 +1,20 @@
+pragma solidity >= 0.5.0 < 0.6.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper {
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= level);
+        _;
+    }
+
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint _zombieId, uint calldata _newDna) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+}
